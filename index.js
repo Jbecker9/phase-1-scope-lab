ffc6701cb58bc0368af16e14090e679aed570bf1
Jared <jbeckersalax9@aol.com>
2021-12-03 14:28:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var customerName = 'bob'
 
 function upperCaseCustomerName(){
-    return customerName = 'BOB'
+    return customerName = 'BOB';
 }
 
+var bestCustomer
+function setBestCustomer(){
+    return bestCustomer = 'not bob';
+}
 
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'tom'
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'TOM';
+}
